1ff37ad2870eecd8a6580fcc3cbf2de2b92968ea
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2022-11-22 19:43:26 +0100

    Use preprocessor directives to reduce the verbose casting

--- LinkedList.c
+++ LinkedList.c
@@ -2,26 +2,30 @@
 #include <stdio.h>
 #include "Functions.h"
 
+#define toList (struct list_node **)
+#define toNode (struct list_node *)
+#define toStringItem (struct string_item *)
+
 int main() {
     struct string_item *my_linked_list = NULL;
-    struct string_item *item = string_item_new("Test001");
-    list_append((struct list_node **)&my_linked_list, (struct list_node *)string_item_new("Hello World"));
-    list_append((struct list_node **)&my_linked_list, (struct list_node *)item);
-    list_append((struct list_node **)&my_linked_list, (struct list_node *)string_item_new("Test002"));
-    list_append((struct list_node **)&my_linked_list, (struct list_node *)string_item_new("Last Item of the Linked List"));
+    struct string_item *new_item = string_item_new("Test001");
+    list_append(toList&my_linked_list, toNode string_item_new("Hello World"));
+    list_append(toList&my_linked_list, toNode new_item);
+    list_append(toList&my_linked_list, toNode string_item_new("Test002"));
+    list_append(toList&my_linked_list, toNode string_item_new("Last Item of the Linked List"));
     struct string_item *string_item = my_linked_list;
     printf("==========LIST BEFORE REMOVE==========\n");
     while (string_item) {
         printf("%s\n", string_item->string);
-        string_item = (struct string_item *)string_item->node.next;
+        string_item = toStringItem string_item->node.next;
     }
-    
-    list_remove((struct list_node **)&my_linked_list, (struct list_node *)item);
+
+    list_remove(toList&my_linked_list, toNode new_item);
     string_item = my_linked_list;
     printf("==========LIST AFTER REMOVE==========\n");
     while (string_item) {
         printf("%s\n", string_item->string);
-        string_item = (struct string_item *)string_item->node.next;
+        string_item = toStringItem string_item->node.next;
     }
     return 0;
 }
