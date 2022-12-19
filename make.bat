9b9bb29b552baefd029ee171ca19d8d56602b711
Lorenzo Guerrini <lollo.guerrini@gmail.com>
2022-12-19 14:30:55 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -8,4 +8,4 @@ if exist "%BAT_BASE_PATH%library" (
 
 MD "%BAT_BASE_PATH%library"
 
-clang -shared -o "%BAT_BASE_PATH%library/linkedlist.dll" -I "%BAT_BASE_PATH%include" -I "%BAT_BASE_PATH%examples/include" "%BAT_BASE_PATH%src/LinkedList.c" "%BAT_BASE_PATH%examples/src/StringItem.c" "%BAT_BASE_PATH%examples/src/main.c"
+clang -shared -o "%BAT_BASE_PATH%library/linkedlist.dll" -I "%BAT_BASE_PATH%include" "%BAT_BASE_PATH%src/LinkedList.c"
