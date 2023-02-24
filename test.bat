0c731a07abbdc6736031bae033b6f176ffeada9e
LorenzoGuerriniSTEB <guerrini@stebsrl.it>
2023-02-24 11:34:12 +0100

    Update test.bat

--- test.bat
+++ test.bat
@@ -9,3 +9,11 @@ IF EXIST "%BAT_BASE_PATH%bin" (
 MD "%BAT_BASE_PATH%bin"
 
 clang -o "%BAT_BASE_PATH%bin\test.exe" -I "%BAT_BASE_PATH%include" "%BAT_BASE_PATH%src\*.c" "%BAT_BASE_PATH%test\*.c"
+
+IF EXIST "%BAT_BASE_PATH%bin\test.exp" (
+    DEL /Q "%BAT_BASE_PATH%bin\test.exp"
+)
+
+IF EXIST "%BAT_BASE_PATH%bin\test.lib" (
+    DEL /Q "%BAT_BASE_PATH%bin\test.lib"
+)
