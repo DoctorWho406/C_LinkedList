b95bf3e662e4bc07aca74455c3053bd3dab9fee5
LorenzoGuerriniSTEB <guerrini@stebsrl.it>
2023-02-24 11:31:51 +0100

    Fix text bat

--- test.bat
+++ test.bat
@@ -8,4 +8,4 @@ IF EXIST "%BAT_BASE_PATH%bin" (
 
 MD "%BAT_BASE_PATH%bin"
 
-clang -o "%BAT_BASE_PATH%bin\Test.exe" -I "%BAT_BASE_PATH%include" "%BAT_BASE_PATH%test\*.c"
+clang -o "%BAT_BASE_PATH%bin\test.exe" -I "%BAT_BASE_PATH%include" "%BAT_BASE_PATH%src\*.c" "%BAT_BASE_PATH%test\*.c"
